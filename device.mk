5a798ffa4c9a0eec3b7e92ffd09453de0128d731
EpicLPer <epiclper7@gmail.com>
2026-08-31 13:43:40 +1000

    talkman: leftover audio acdb/policy MATCH TAS2553 QUAT acdb 14.
    HAL defaults SPEAKER_PROTECTED to 124. XML already remaps SPEAKER / VOICE_SPEAKER / PROTECTED to 14. Add SPEAKER_SAFE, SPEAKER_REVERSE, VOICE_SPEAKER_HFP so those backends stay on Speaker_cal_8994.acdb id 14 (same as mixer QUAT_MI2S). Copy leftover audio_policy XML to vendor like mixer and platform_info. Do not edit mixer_paths.xml (66c7d50).

--- device.mk
+++ device.mk
@@ -124,7 +124,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes_drc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_policy_volumes_drc.xml \
+    $(LOCAL_PATH)/audio/audio_policy_volumes_drc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes_drc.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9330.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sound_trigger_mixer_paths_wcd9330.xml \
